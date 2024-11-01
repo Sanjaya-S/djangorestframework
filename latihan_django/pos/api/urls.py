a641fa189e7d63e308e29abdde4eaaf78ab58aaf
stephen <sanjayastephen555@gmail.com>
2024-11-01 19:32:04 +0700

    first commit

---
+++ latihan_django/pos/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from api import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+app_name='api'
+urlpatterns = [
+    #path('api/vl/login', LoginView.as_view()),
+    #path('api/vl/logiout', LogoutView.as_view()),
+    #path('api/vl/register', RegisterWaitressApi.as_view()),
+]
---
+++ latihan_django/pos/pos_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class user(AbstractUser):
+    is_waitress = models.BooleanField(default = False)
+    is_cashier = models.BooleanField(default = False)
+    is_kitchen = models.BooleanField(default = False)
+
+class TableResto(models.Model):
+    status_choices = (
+        ('Aktif', 'Aktif'),
+        ('Tidak Aktif', 'Tidak Aktif'),
+    )
+    status_table_choices = (
+        ('Kosong', 'Kosong'),
+        ('Terisi', 'Terisi'),
+    )
+    code = models.CharField(max_length = 20)
+    name = models.CharField(max_length = 100)
+    capacity = models.IntegerField(max_length = 0)
+    table_status = models.CharField(max_length = 15, choices = status_table_choices, default = 'Kosong')
+    status = models.CharField(max_length = 15, choices = status_choices, default = 'Aktif')
+    user_create = models.ForeignKey(User, related_name = 'user_create_table_resto', blank = True, null = True, on_delete = models.)
+    user_update = models.ForeignKey(User, related_name = 'user_update_table_resto', blank = True, null = True, on_delete = models.)
+
+    def __str__(self):
+            return str(self.username) + ' ' + str(self.first_name) + ' ' + str(self.last_name)
